0a496f2328af88d71f69af0437eeb6f1a91f0b54
TatianaYudakova <yudakovatl@gmail.com>
2018-03-12 17:49:59 +0400

    создание документа (добавлене еще 4х файлов)

---
+++ src/app/document-create/document-create.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {DocumentService} from "../service/document.service";
+import {Document} from "../shared/document";
+import {DOCUMENTS} from "../shared/mock-documents";
+import {Category} from "../shared/category";
+
+@Component({
+  selector: 'app-document-create',
+  templateUrl: './document-create.component.html',
+  styleUrls: ['./document-create.component.css'],
+  providers: [DocumentService]
+})
+export class DocumentCreateComponent implements OnInit {
+
+  constructor(private documentService: DocumentService) { }
+
+  @Input() isAddDocument: boolean;
+  @Output() onChangedDoc = new EventEmitter<boolean>();
+
+  private _category_dc: Category;
+
+  @Input() set category_dc(value: Category){
+    this._category_dc = value;
+  }
+
+  get category_dc(): Category {
+    return this._category_dc;
+  }
+
+  documents: Document[] = [];
+
+  nameDocument = '';
+  contentDocument = '';
+
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments();
+  }
+
+  addDocument() {
+    this.documentService.addDocument(0, this.nameDocument, this.contentDocument);
+    this._category_dc.id_doc.push(DOCUMENTS[DOCUMENTS.length-1].id);
+  }
+
+  closeModal(increased: any) {
+    this.isAddDocument = false;
+    this.onChangedDoc.emit(true);
+
+    this.nameDocument = '';
+    this.contentDocument = '';
+  }
+
+}
